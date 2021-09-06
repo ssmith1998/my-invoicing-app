50c80d8b709a3c558c904f55fb6949a0cf5dff0c
Sean Smith <smith.sean1998@gmail.com>
2021-09-06 21:21:14 +0100

    changes

--- src/pages/Invoices.vue
+++ src/pages/Invoices.vue
@@ -7,7 +7,57 @@
       :rows="invoices"
       :columns="columns"
       row-key="id"
-    />
+    >
+    <template v-slot:top>
+      <q-icon name="add_circle" style="font-size:45px; cursor:pointer;" color="positive" @click="addInvoiceDialog = true"/>
+    </template>
+    
+    </q-table>
+    <q-dialog v-model="addInvoiceDialog">
+      <q-card style="width:90%;" >
+          <q-card-section>
+            <h1 class="text-h5 q-ma-none">Add Invoice</h1>
+          </q-card-section>
+          <q-card-section>
+            <form>
+            <div class="row">
+              <div class="col-xs-12 col-sm-4">
+                <q-select
+                label="Customer"
+                v-model="invoice.customer"
+                >
+
+                </q-select>
+              </div>
+              <div class="col-xs-12 col-sm-4">
+                <q-select
+                label="Template"
+                v-model="invoice.template"
+                >
+
+                </q-select>
+              </div>
+              <div v-if="!invoice.template" class="col-xs-12 col-sm-4">
+                <q-input
+                label="Name"
+                v-model="invoice.name"
+                ></q-input>
+              </div>
+              <div class="col-xs-12 col-sm-4">
+                <q-input
+                label="Due Date"
+                v-model="invoice.due_date"
+                type="date"
+                ></q-input>
+              </div>
+            </div>
+            </form>
+          </q-card-section>
+
+      </q-card>
+
+
+    </q-dialog>
   </q-page>
 </template>
 
@@ -15,6 +65,13 @@
 export default {
 data () {
     return{
+      invoice: {
+        customer:'',
+        template:'',
+        name:'',
+        due_date: ''
+      },
+      addInvoiceDialog: false,
         columns: [
     {
     name: 'id',
@@ -46,5 +103,7 @@ data () {
 </script>
 
 <style>
-
+.q-table__top  {
+  justify-content:flex-end;
+}
 </style>
